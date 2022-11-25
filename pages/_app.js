e3bbe5c157fe240bb15361c67f031dae6853dffa
imkarthi16 <karthiveera04@gmail.com>
2022-11-25 17:21:03 +0530

    Working with travel blog | update custome mui theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
+
+
+const theme = createTheme({
+  palette: {
+    text: {
+      primary: "#708238",
+    },
+  },
+  typography: {
+    fontFamily: "Poppins",
+    h2: {
+      fontFamily: "Alex Brush",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
