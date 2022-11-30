ba8c6a8266448f14cfba110fede82a628329208e
imkarthi16 <karthiveera04@gmail.com>
2022-11-30 16:21:32 +0530

    Working with travel blog | Create dynamic path using getStaticPaths

--- pages/trip/[slug].js
+++ pages/trip/[slug].js
@@ -1,5 +1,24 @@
 import React from 'react'
 
+
+export const getStaticPaths = async()=>{
+  const response = await client.getEntries({
+    content_type : 'trips'
+  })
+  // {props : {slug:val}}
+  const paths = response.items.map((trip)=>{
+    return{
+      params : {
+        slug : trip.fields.slug
+      }
+    }
+  })
+  return{
+    paths,
+    fallback: false
+  }
+} 
+
 const TripDetail = () => {
   return (
     <div>TripDetail Page</div>
