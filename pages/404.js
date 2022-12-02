78d7845c8f9ebdde9165f5a548cf9a825519acbf
imkarthi16 <karthiveera04@gmail.com>
2022-12-02 17:46:50 +0530

    Working with travel blog | Create 404 component styled jsx

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,27 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const PageNotFound = () => {
   return (
     <div className="container">
-        <h1>Oops! Page Not Found</h1>
-        <Link href='/'>Go To Home Page</Link>
-    </div>
-  )
-}
+      <h1>Oops! Page Not Found</h1>
+      <Link href="/">Go To Home Page</Link>
 
-export default PageNotFound
+      <style jsx>
+            {`
+                .container {
+                    max-width:1200px;
+                    display:flex;
+                    justify-content:center;
+                    align-items:center;
+                    flex-direction:column;
+                    padding:100px 50px;
+                    text-align:center;
+                }
+            `}
+        </style>
+    </div>
+  );
+};
+
+export default PageNotFound;
