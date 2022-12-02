485cf402ec7af747bf8b674d626c7dfcc1a6d5f5
imkarthi16 <karthiveera04@gmail.com>
2022-12-02 17:53:45 +0530

    Working with travel blog | Automatically goes to homepage from 404 page timing

--- pages/404.js
+++ pages/404.js
@@ -1,25 +1,35 @@
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 const PageNotFound = () => {
+  //Automatically goes to homepage from 404 page timing
+  const route = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      route.push("/");
+    }, 5000);
+    clearTimeout(timer);
+  }, []);
   return (
     <div className="container">
       <h1>Oops! Page Not Found</h1>
       <Link href="/">Go To Home Page</Link>
 
       <style jsx>
-            {`
-                .container {
-                    max-width:1200px;
-                    display:flex;
-                    justify-content:center;
-                    align-items:center;
-                    flex-direction:column;
-                    padding:100px 50px;
-                    text-align:center;
-                }
-            `}
-        </style>
+        {`
+          .container {
+            max-width: 1200px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            padding: 100px 50px;
+            text-align: center;
+          }
+        `}
+      </style>
     </div>
   );
 };
